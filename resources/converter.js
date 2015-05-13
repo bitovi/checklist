4571162de0dcbd73fe22045621968d49af1d2f5b
Brian Moschel <brian.moschel@gmail.com>
2015-05-13 02:41:44 -0500

    adding success formula

--- resources/converter.js
+++ resources/converter.js
@@ -77,7 +77,58 @@ $(function() {
   $('body').on('submit', 'form', function(ev){
     var data = $('form').formParams();
     ev.preventDefault();
-    $('#checklist').html("<div class='answers'>"+JSON.stringify(data)+"</div>");
-    $("#links").remove();
+    renderScore(data);
   })
+
+  var answers = {
+    "Do all employees go through a technical training?": 0.46,
+    "What is the project\'s vision?": 0.45,
+    "How long until something can be released?": 0.48,
+    "Does the company have outings?": 0.45,
+    "Is user testing done?": 0.45,
+    "The following documents are created:": 0.44,
+    "There are code reviews": 0.28,
+    "Are there unit tests?": 0.2,
+    "Is there documentation for the code?": 0.28,
+    "Continuous integration": 0.2,
+    "The following environments exist": 0.2
+  }
+
+  var getScore = function(data){
+    var total = 0, count = 0;
+    for (var key in answers){
+      total += answers[key];
+      count++;
+    }
+    var points = 0;
+    for(var key in data) {
+      if(answers[key] && data[key] !== "none" && data[key] !== "No"){
+        points+=answers[key];
+      }
+    }
+    return parseInt(points/total*100, 10);
+  }
+
+  var renderScore = function(data){
+    var score = getScore(data);
+    $(".links").remove();
+    $('#checklist').html(can.view("resources/doughnut.mustache")({
+        score: score
+    }));
+    $("#doughnutChart").drawDoughnutChart([
+      { 
+        title: "Yes, beyond your wildest dreams :)",
+        value : score,  
+        color: "#4BA658" 
+      },
+      { 
+        title: "No, burning flames are in your future :(",
+        value:  100-score,   
+        color: "#D93B48" 
+      }
+    ], {
+
+    });
+    $('#checklist').append("<div class='answers'>"+JSON.stringify(data)+"</div>");
+  }
 });
