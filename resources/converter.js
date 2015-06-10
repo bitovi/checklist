26897b7733520b43396c3941ac528785319a39f1
Juan Orozco <juanthedesigner@gmail.com>
2015-06-09 23:40:23 -0400

    Added ability to paste in previous results and link to results.

--- resources/converter.js
+++ resources/converter.js
@@ -9,7 +9,7 @@ $(function() {
         caption: getLabel(current),
         type: name,
         options: {},
-        name: current.label
+        name: current.name ? current.name : current.label,
       };
       $.each(current.values, function(index, value) {
         dform.options[value] = value;
@@ -25,7 +25,7 @@ $(function() {
         html: {
           caption: getLabel(current),
           type: current.type,
-          name: current.label,
+          name: current.name ? current.name : current.label,
           class: 'form-control'
         }
       };
@@ -34,7 +34,7 @@ $(function() {
       var dform = {
         caption: getLabel(current),
         type: 'fieldset',
-        name: current.label,
+        name: current.name ? current.name : current.label,
         id: current.id,
         html: []
       };
@@ -53,6 +53,80 @@ $(function() {
     converter = converter || converters.default;
     return converter(data, depth ? depth + 1 : 0);
   };
+  
+  var PasteBucket = function () {
+    var pasteBucket = {
+      visible: false,
+      toggle: function (force) {
+        if (typeof force !== 'undefined') {
+          this.visible = force;
+        } else {
+          this.visible = !this.visible;
+        }
+        
+        if (this.visible) {
+          $('.paste-tool').show();
+        } else {
+          $('.paste-tool').hide();
+        }
+      },
+      processData: function () {
+        var pasteData = $('.paste-tool .paste-bucket').val(),
+            self = this;
+
+        if (pasteData.length === 0) {
+          $('.paste-bucket').parent('.form-group').addClass('has-error');
+          return false;
+        }
+        self.loadDataFromString(pasteData);
+        self.toggle();
+      },
+      checkParams: function () {
+        
+        var searchTerms = new can.List(window.location.search.replace('?','').split('&')),
+          pastedLink;
+        searchTerms.each(function (item) {
+          var params = item.split('=');
+          if (params[0] === 'checklist' && params[1]) {
+            pastedLink = decodeURI(params[1]);
+          }
+        });
+        
+        if (pastedLink) {
+          this.loadDataFromString(pastedLink);
+        }
+      },
+      loadDataFromString: function (dataString) {
+        var pasteDataMap = new can.Map(JSON.parse(dataString))
+        pasteDataMap.each(function (val, key, item) {
+            var $target = can.$('[name='+key+']');
+            if ($target.eq(0).is('[type=checkbox]')) {
+                val.each(function (chkboxVal) {
+                    $target.filter('[value="'+chkboxVal+'"]').prop('checked', true);
+                });
+                
+                return true;
+            }
+            if ($target.eq(0).is('[type=radio]')) {
+                $target.filter('[value="'+val+'"]').prop('checked', true);
+                return true;
+            }
+            $target.val(val);
+            
+        });
+      }
+    }
+    return pasteBucket;
+  }
+  var pasteBucket = new PasteBucket();
+    
+  $('.paste-tool .btn-default').on('click', function () {
+    pasteBucket.processData();
+  });
+  pasteBucket.toggle(false);//close the bucket
+  $('.open-paste-tool-js').on('click', function () {
+    pasteBucket.toggle();
+  });
 
   $.getJSON('questions.json').then(function(data) {
     var questions = data.questions;
@@ -72,6 +146,9 @@ $(function() {
     });
 
     $('#checklist').append("<div class='text-center'><input class='btn btn-lg btn-primary' type='submit' value='Submit' /></div>");
+    
+    //check if linking into results
+    pasteBucket.checkParams();
   });
 
   $('body').on('submit', 'form', function(ev){
@@ -81,17 +158,17 @@ $(function() {
   })
 
   var answers = {
-    "Do all employees go through a technical training?": 0.46,
-    "What is the project\'s vision?": 0.45,
-    "How long until something can be released?": 0.48,
-    "Does the company have outings?": 0.45,
-    "Is user testing done?": 0.45,
-    "The following documents are created:": 0.44,
-    "There are code reviews": 0.28,
-    "Are there unit tests?": 0.2,
-    "Is there documentation for the code?": 0.28,
-    "Continuous integration": 0.2,
-    "The following environments exist": 0.2
+    "do-all-employees-go-through-a-technical-": 0.46,//Do all employees go through a technical training?
+    "what-is-the-projects-vision": 0.45,//What is the project\'s vision?
+    "how-long-until-something-can-be-released": 0.48,//How long until something can be released?
+    "does-the-company-have-outings": 0.45,//Does the company have outings?
+    "is-user-testing-done": 0.45,//Is user testing done?
+    "the-following-documents-are-created": 0.44,//The following documents are created:
+    "there-are-code-reviews": 0.28,//There are code reviews
+    "are-there-unit-tests": 0.2,//Are there unit tests?
+    "is-there-documentation-for-the-code": 0.28,//Is there documentation for the code?
+    "continuous-integration": 0.2,//Continuous integration
+    "the-following-environments-exist": 0.2//The following environments exist
   }
 
   var getScore = function(data){
